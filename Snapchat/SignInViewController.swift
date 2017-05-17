d66810308fe27695c4d878ca8db89fa7130bbd90
Brandon Viertel <bviertel@gmail.com>
2017-05-17 19:32:17 -0400

    CHANGE: Linked all items on sign in page to code

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var logoLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signInTapped(_ sender: Any) {
     }
-
 
 }
 
