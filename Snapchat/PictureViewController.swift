3c47a436df662280e9506fb4e47f9da28d29efad
Brandon Viertel <bviertel@gmail.com>
2017-05-18 08:31:44 -0400

    CHANGE: Sending images to Firebase with unique ID, SelectUserVC, visual adjustments and adds

--- Snapchat/PictureViewController.swift
+++ Snapchat/PictureViewController.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseStorage
+import FirebaseDatabase
+import FirebaseAuth
+
+
 
 class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -36,6 +43,8 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         imageView.image = image
         
+        // Because we have a gray background in our original ImageView to show that there IS a spot for a picture, we need to change it back to transparent so that we don't have any gray edges if the picture isn't the same size
+        
         imageView.backgroundColor = UIColor.clear
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -44,6 +53,49 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
     }
     
     @IBAction func nextTapped(_ sender: Any) {
+        
+        
+        // Disables button while processing upload
+        
+        self.nextButton.isEnabled = false
+        
+        // Reference to the image folder in Firebase storage
+        
+        let imagesFolder = Storage.storage().reference().child("images")
+        
+        // Turns image into data
+        
+        //let imageData = UIImagePNGRepresentation(imageView.image!)!
+        
+        // Change to JPG instead
+        
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.1)!
+        
+        // Creates the file with a NSUUID (Unique ID) converted to String and puts it in the images folder in Firebase
+        
+        imagesFolder.child("\(NSUUID().uuidString).jpg").putData(imageData, metadata: nil) { (metadata, error) in
+            
+            print("Upload Attempted")
+            
+            if error != nil {
+                
+                print("ERROR: \(error)")
+                
+            } else {
+                
+                print("Upload Successful")
+
+                // Path to file
+                
+                print(metadata?.downloadURL())
+                
+                // If no errors, THEN perform the segue
+                
+                self.performSegue(withIdentifier: "selectUserSegue", sender: nil)
+            
+            }
+        }
+        
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
@@ -53,9 +105,16 @@ class PictureViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         imagePicker.sourceType = .photoLibrary
         
+        // By default, when a picture is selected it would go to the editing screen
+        
         imagePicker.allowsEditing = false
         
         present(imagePicker, animated: true, completion: nil)
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ 
+
     }
 }
---
+++ Snapchat/SelectUserViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SelectUserViewController.swift
+//  Snapchat
+//
+//  Created by Ann Marie Seyerlein on 5/18/17.
+//  Copyright © 2017 Brandon. All rights reserved.
+//
+
+import UIKit
+
+// Need to set up numrowssection and cellforrowat as well as delegate and datasource otherwise error will be thrown
+
+class SelectUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        
+    }
+
+
+}
